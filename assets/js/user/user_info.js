80a065d737772acfda7c807a8b2d59f049ff107c
pan-jun-an <mikyypan@163.com>
2020-07-30 16:42:50 +0800

    完成了修改资料页面

--- assets/js/user/user_info.js
+++ assets/js/user/user_info.js
@@ -1,59 +1,25 @@
 $(function () {
-
-   var form = layui.form;
-   form.verify({
-      nickname: function (value) {
-         if (value.length > 6)  return '昵称只能6-12位之间';
-      }
-   })
-
-
-
-
+   //渲染资料页面数据
    $.ajax({
       type: 'get',
       url: '/my/userinfo',
       success: function (res) {
          if (res.status !== 0) return;
-         console.log(res);
          localStorage.setItem('id', res.data.id);
-         $('#username').val(res.data.username);
-         $('#nickname').val(res.data.nickname);
-         $('#email').val(res.data.email);
-
+         layui.form.val('fUInfo',res.data);  //快速赋值
          $('form').on('submit', function (e) {
             e.preventDefault();
-            //获取用户id
-            let id = localStorage.getItem('id');
-            $.ajax({
-               type: 'POST',
-               url: '/my/userinfo',
-               data: {
-                  id: id,
-                  nickname: $('#nickname').val(),
-                  email: $('#email').val()
-               },
-               success: function (res) {
-                  if (res.status !== 0) return '修改用户信息失败!'
-                  console.log('修改成功');
+            //限制用户输入昵称长度
+            layui.form.verify({
+               nickname: function (value) {
+                  if (value.length > 6) return '昵称只能6-12位之间';
                }
-            })
+            });
+            let id = localStorage.getItem('id');
+            $.post('/my/userinfo',{id:id,
+               nickname: $('#nickname').val(),
+               email: $('#email').val()})
          })
-
-
-
       }
    })
-
-
-
-
-
-
-
-
-
-
-
-
 }) //end
